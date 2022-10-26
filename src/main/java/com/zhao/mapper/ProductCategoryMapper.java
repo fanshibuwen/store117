42f70b0cf9fed66f42f4383f24bd5906ebc4cbda
zhaol <1976915593@qq.com>
2022-10-26 21:49:27 +0800

    增加一点，因为今天事比较多 新增dao、pom、配置文件，增加逻辑删除字段

---
+++ src/main/java/com/zhao/mapper/ProductCategoryMapper.java
@@ -0,0 +1,13 @@
+package com.zhao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zhao.entity.ProductCategory;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Author zhaoL
+ * @Time 2022/10/26 21:08
+ */
+@Repository
+public interface ProductCategoryMapper extends BaseMapper<ProductCategory> {
+}
