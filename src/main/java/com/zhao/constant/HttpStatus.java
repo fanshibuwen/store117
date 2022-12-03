37c0258abddb5708a4662483e52e0917e8ed5134
zhaol <1976915593@qq.com>
2022-12-03 21:16:53 +0800

    http状态

---
+++ src/main/java/com/zhao/constant/HttpStatus.java
@@ -0,0 +1,17 @@
+package com.zhao.constant;
+
+/**
+ * http 响应状态码
+ * @Author zhaoL
+ * @Time 2022/12/2 21:13
+ */
+public class HttpStatus {
+
+    public static final int SUCCESS = 200;
+
+    public static final int NOT_FOUND = 404;
+
+    public static final int ERROR = 500;
+
+    public static final int FORBIDDEN = 403;
+}
